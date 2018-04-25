ed977455140f3471766b84a1452a49b0291b726c
Suttor <daniel.suttor@student.uibk.ac.at>
2018-04-25 18:33:02 +0200

    bild + linie + polygon hinzugefuegt

--- marker.js
+++ marker.js
@@ -1,6 +1,9 @@
 
 let myMap = L.map("mapdiv"); // http://leafletjs.com/reference-1.3.0.html#map-l-map
-myLayers = {    
+
+let markerGroup = L.featureGroup();
+
+let myLayers = {    
     osm : L.tileLayer ( // http://leafletjs.com/reference-1.3.0.html#tilelayer-l-tilelayer
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
 	{ 
@@ -44,6 +47,7 @@ myLayers = {
 
 myMap.addLayer(myLayers.geolandbasemap); // http://leafletjs.com/reference-1.3.0.html#map-addlayer
 
+
 let myMapControl = L.control.layers({  // http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
     "Openstreetmap" : myLayers.osm,
     "Basemap.at" : myLayers.geolandbasemap,
@@ -52,6 +56,7 @@ let myMapControl = L.control.layers({  // http://leafletjs.com/reference-1.3.0.h
     "Orthophoto 30cm" : myLayers.bmaporthofoto30cm,    
     
 },{"Basemap overlay" : myLayers.bmapoverlay,
+    "Marker": markerGroup
 },
 {collapsed:false  // http://leafletjs.com/reference-1.3.0.html#control-layers-collapsed
 }
@@ -71,20 +76,45 @@ maxWidth:200 // http://leafletjs.com/reference-1.3.0.html#control-scale-maxwidth
 
 ).addTo(myMap); 
 
+
 const uni = [47.264,11.385]; 
 const usi = [47.257,11.356]; 
-const technik = [47.263,11.348]; 
+const technik = [47.263,11.348];
+const patscherkofel = [47.2086, 11.4605];
+const patscherkofelbild = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Luftbild_Patscherkofel.jpg/1024px-Luftbild_Patscherkofel.jpg"
+const iglsCoords = [47.2308, 11.4089];
+
+const latlngs = [
+    patscherkofel,
+    iglsCoords
+];
+
+let lift = L.polyline(latlngs, {color: 'red'}).addTo(myMap)
+   
+
+myMap.addLayer(markerGroup);
+
 const markeroptions = {
     title: "Uni IBK",
     opacity: 0.5,
     draggable: true
 };
 
-L.marker(uni, markeroptions).addTo(myMap);
+L.marker(uni, markeroptions).addTo(markerGroup);
 
-L.marker(usi, markeroptions).addTo(myMap);
+L.marker(usi, markeroptions).addTo(markerGroup);
 
-L.marker(technik, markeroptions).addTo(myMap);
+L.marker(technik, markeroptions).addTo(markerGroup);
 
+L.marker(iglsCoords).addTo(markerGroup);
 
-myMap.setView(uni, 13);
+let patscherkoflMarker = L.marker(patscherkofel).addTo(markerGroup);
+patscherkoflMarker.bindPopup("<p> Patscherkofel</p><img style= 'width:200px' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Luftbild_Patscherkofel.jpg/1024px-Luftbild_Patscherkofel.jpg' alt='Patscherkofl'/>")
+
+let  uniPolyon = L.polygon([uni, usi, technik]);
+myMap.addLayer(uniPolyon);
+
+myMap.fitBounds(markerGroup.getBounds());
+
+// myMap.setView(uni, 13);
+
