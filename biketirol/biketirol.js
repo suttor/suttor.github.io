4e0415d52b5e1efd3446aadf7e0b4461d54498bc
Daniel Suttor <sutta@live.com>
2018-05-13 20:43:55 +0200

    neue daten + erste anpassungen im html

---
+++ biketirol/biketirol.js
@@ -0,0 +1,28 @@
+/*
+    Vorbereitung: GPX Track herunterladen und nach GeoJSON konvertieren
+    -------------------------------------------------------------------
+    Datenquelle https://www.data.gv.at/suche/?search-term=bike+trail+tirol&searchIn=catalog
+    Download Einzeletappen / Zur Ressource ...
+    Alle Dateien im unterverzeichnis data/ ablegen
+    Die .gpx Datei der eigenen Etappe als etappe00.gpx speichern
+    Die .gpx Datei über https://mapbox.github.io/togeojson/ in .geojson umwandeln und als etappe00.geojson speichern
+    Die etappe00.geojson Datei in ein Javascript Objekt umwandeln und als etappe00.geojson.js speichern
+
+    -> statt 00 natürlich die eigene Etappe (z.B. 01,02, ...25)
+*/
+
+// eine neue Leaflet Karte definieren
+
+// Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
+// WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
+
+// Maßstab metrisch ohne inch
+
+// Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/
+
+// GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
+// Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)
+
+// Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
+
+// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
